e6f4146109fca84fcf084866899f133ebec947c1
Halim Labouiti <hlabouit@e3r9p18.1337.ma>
2023-10-01 04:46:50 +0100

    getting familiarized with classes, objects, and stuff

---
+++ Exercise 01/80s_PhoneBook.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   80s_PhoneBook.cpp                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hlabouit <hlabouit@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/30 20:39:33 by hlabouit          #+#    #+#             */
+/*   Updated: 2023/10/01 04:38:18 by hlabouit         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include"80s_PhoneBook.hpp"
+
+int main()
+{   
+    class ZARA {
+        public:
+            std::string jeans_90s;
+            int size;
+            void method() {
+                std::cout<< "90's staright fit jeans" << std::endl;
+            }
+    };
+
+    ZARA jean1;
+    jean1.method();
+}
