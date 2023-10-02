d6f8a1c45bed88aa65905d991fa8cb341ddd7047
Halim Labouiti <hlabouit@e3r8p2.1337.ma>
2023-10-02 09:09:43 +0100

    80's PhoneBook in the making

--- Exercise 01/80s_PhoneBook.cpp
+++ Exercise 01/80s_PhoneBook.cpp
@@ -6,23 +6,14 @@
 /*   By: hlabouit <hlabouit@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 20:39:33 by hlabouit          #+#    #+#             */
-/*   Updated: 2023/10/01 04:38:18 by hlabouit         ###   ########.fr       */
+/*   Updated: 2023/10/02 08:04:14 by hlabouit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"80s_PhoneBook.hpp"
 
 int main()
-{   
-    class ZARA {
-        public:
-            std::string jeans_90s;
-            int size;
-            void method() {
-                std::cout<< "90's staright fit jeans" << std::endl;
-            }
-    };
+{
+    
 
-    ZARA jean1;
-    jean1.method();
 }
--- Exercise 01/80s_PhoneBook.hpp
+++ Exercise 01/80s_PhoneBook.hpp
@@ -6,7 +6,7 @@
 /*   By: hlabouit <hlabouit@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 21:31:33 by hlabouit          #+#    #+#             */
-/*   Updated: 2023/10/01 03:38:45 by hlabouit         ###   ########.fr       */
+/*   Updated: 2023/10/02 09:08:56 by hlabouit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,25 @@
 
 #include<iostream>
 #include<string>
+
+class Contact {
+	public:
+		std::string first_name;
+		std::string last_name;
+		std::string nick_name;
+		std::string phone_number;
+		std::string darkest_secret;
+};
+
+class PhoneBook {
+	public:
+		Contact contacts[8];
+		void fill_data(std::string field);
+};
+
+
+
+
 
 
 #endif
