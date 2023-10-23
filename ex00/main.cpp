ec6f38a342878d1d0e7b59f387c1259a5a328b9a
FRUIT-DU-DEMON <hlabouit@student.1337.ma>
2023-10-23 02:34:17 +0100

    annuncing new Zombie is done

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,12 +1,41 @@
-/* ************************************************************************** */
+/******************************************************************************/
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hlabouit <hlabouit@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/21 00:41:18 by hlabouit          #+#    #+#             */
-/*   Updated: 2023/10/21 00:41:18 by hlabouit         ###   ########.fr       */
+/*   Created: 2023/10/21 17:40:23 by hlabouit          #+#    #+#             */
+/*   Updated: 2023/10/23 00:48:24 by hlabouit         ###   ########.fr       */
 /*                                                                            */
-/* ************************************************************************** */
+/******************************************************************************/
 
+#include<iostream>            
+#include<string>  
+#include<iomanip>
+#include<ctype.h>  
+#include <cstdlib>
+
+class Zombie {
+	private :
+		std::string name;
+	public :
+		void announce();
+		Zombie(std::string zombie_name);
+};
+
+Zombie::Zombie(std::string zombie_name)
+{
+	this->name = zombie_name;
+}
+
+void Zombie::announce()
+{
+	std::cout<< this->name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
+
+int main()
+{
+	Zombie z1("MAKINCH_DDA7K");
+	z1.announce();
+}
