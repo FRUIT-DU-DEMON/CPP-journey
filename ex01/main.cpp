22db9c701f618524fb0b94b1eb3558095cc9d813
FRUIT-DU-DEMON <hlabouit@student.1337.ma>
2023-10-24 16:37:54 +0000

    horde of zombies is done

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hlabouit <hlabouit@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 01:53:54 by hlabouit          #+#    #+#             */
-/*   Updated: 2023/10/24 05:12:49 by hlabouit         ###   ########.fr       */
+/*   Updated: 2023/10/24 16:36:42 by hlabouit         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -45,17 +45,20 @@ Zombie *zombieHorde(int N, std::string name)
 {
     Zombie *zombizz = new Zombie[N];
 	for (int i = 0; i < N; i++)
-	{
 		zombizz[i].set_name(name);
-	}
 	return (zombizz);
 }
 
 int main()
 {
-	Zombie *zombiizz = zombieHorde(10, "run_dumbass");
-	for (int i = 0; i < 10; i++)
-		zombiizz[i].announce();
-	delete[] zombiizz;
+	int N = 13;
+	std::string name = "run_dumbass";
+	if (N > 0)
+	{
+		Zombie *zombiizz = zombieHorde(N, name);
+		for (int i = 0; i < N; i++)
+			zombiizz[i].announce();
+		delete[] zombiizz;
+	}
 	return (0);
 }
