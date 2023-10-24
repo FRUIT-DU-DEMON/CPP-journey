f294578bf6d9a882f882ff232157f40a891537ce
FRUIT-DU-DEMON <hlabouit@student.1337.ma>
2023-10-24 02:11:27 +0000

    ex01 started

---
+++ ex01/main.cpp
@@ -0,0 +1,35 @@
+/******************************************************************************/
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hlabouit <hlabouit@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/24 01:53:54 by hlabouit          #+#    #+#             */
+/*   Updated: 2023/10/24 02:11:04 by hlabouit         ###   ########.fr       */
+/*                                                                            */
+/******************************************************************************/
+
+#include<iostream>            
+#include<string>  
+#include<iomanip>
+#include<ctype.h>  
+#include <cstdlib>
+
+class Zombie {
+	private :
+		std::string name;
+	public :
+		void announce();
+		Zombie(std::string zombie_name);
+		~Zombie();
+};
+
+Zombie *zombieHorde(int N, std::string name)
+{
+    
+}
+
+int main()
+{
+}
