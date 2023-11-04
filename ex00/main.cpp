51baf065d19ec5b7d4226f85aa141bd8271e4c31
FRUIT-DU-DEMON <hlabouit@student.1337.ma>
2023-11-04 22:43:02 +0000

    fixing the non similar output where getrawbits isn't called correctly

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hlabouit <hlabouit@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 00:56:44 by hlabouit          #+#    #+#             */
-/*   Updated: 2023/11/04 22:24:05 by hlabouit         ###   ########.fr       */
+/*   Updated: 2023/11/04 22:39:40 by hlabouit         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -36,13 +36,13 @@ class Fixed {
 Fixed::Fixed()
 {
 	this->fpn = 0;
-	std::cout << "Default constructor is invoked" << std::endl;
+	std::cout << "Default constructor invoked" << std::endl;
 }
 
 Fixed::Fixed(const Fixed &primary)
 {
 	this->fpn = primary.fpn;
-	std::cout << "Copy constructor is invoked" << std::endl;
+	std::cout << "Copy constructor invoked" << std::endl;
 }
 
 Fixed &Fixed::operator=(const Fixed &primary)
@@ -50,17 +50,38 @@ Fixed &Fixed::operator=(const Fixed &primary)
 	//protection if self-assignment to avoid unnecessary work
 	if (this != &primary)
 		this->fpn = primary.fpn;
-	std::cout << "Copy assignment operator is invoked" << std::endl;
+	std::cout << "Copy assignment operator invoked" << std::endl;
 	return (*this);
 }
 
+int Fixed::getRawBits() const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (this->fpn);
+}
 
+void Fixed::setRawBits(int const raw)
+{
+	this->fpn = raw;
+	std::cout << "setRawBits member function called" << std::endl;
+}
 
+Fixed::~Fixed()
+{
+	std::cout<< "Destructor invoked" << std::endl;
+}
 
 
 
 
 int main()
 {
-	
+	Fixed a;
+	Fixed b( a );
+	Fixed c;
+	c = b;
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+	return 0;
 }
