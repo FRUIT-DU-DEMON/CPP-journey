4f7d00e5a86b683c48a9c24dfb4ef3929589f3e1
FRUIT-DU-DEMON <hlabouit@student.1337.ma>
2023-11-04 20:38:36 +0000

    class Fixed implemented

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,12 +6,12 @@
 /*   By: hlabouit <hlabouit@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 00:56:44 by hlabouit          #+#    #+#             */
-/*   Updated: 2023/11/02 07:47:54 by hlabouit         ###   ########.fr       */
+/*   Updated: 2023/11/04 20:38:04 by hlabouit         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
-// #ifndef HARL_H
-// # define HARL_H
+// #ifndef FIXED_H
+// # define FIXED_H
 // #endif
 #include<fstream>            
 #include<iostream>            
@@ -20,41 +20,25 @@
 #include<ctype.h>  
 #include <cstdlib>
 
-class ttsstt {
-	private:
-		int vall;
-	public:
-		ttsstt(int assign);//constructor
-		ttsstt(const ttsstt &oldObj);//copy constructor
-		ttsstt &operator=(const ttsstt &oldOObj);//Copy assignment operator
-		void print();
-		void change(int changement);
+class Fixed {
+	private :
+		int fpn;
+		static const int fpn_binary = 8;
+	public :
+		Fixed();
+		Fixed(const Fixed &primary);
+		Fixed &operator=(const Fixed &primary);
+		int getRawBits() const;
+		void setRawBits(int const raw);
+		~Fixed();
+		
 };
 
-void ttsstt::change(int changement)
-{
-	this->vall = changement;
-}
 
-void ttsstt::print()
-{
-	std::cout<< this->vall << std::endl;
-}
-
-ttsstt::ttsstt(int assign) : vall(assign) {}
-ttsstt::ttsstt(const ttsstt &oldObj) : vall(oldObj.vall) {}
 
 
 
 int main()
 {
-	ttsstt obj1(100);
-	obj1.print();
-	ttsstt obj2(200);
-	obj2.print();
-	obj2.change(1337);
-	obj2 = obj1;
-	obj1.print();
-	obj2.print();
 	
 }
