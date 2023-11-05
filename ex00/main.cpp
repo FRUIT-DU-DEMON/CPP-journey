7099d73a3d65adc767e0d681dafa7aa4d5861a8c
FRUIT-DU-DEMON <hlabouit@student.1337.ma>
2023-11-05 00:09:58 +0000

    output is similar now + changes the outputs order + call getter instead of assigning primary.value

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hlabouit <hlabouit@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 00:56:44 by hlabouit          #+#    #+#             */
-/*   Updated: 2023/11/04 22:39:40 by hlabouit         ###   ########.fr       */
+/*   Updated: 2023/11/05 00:06:59 by hlabouit         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -35,22 +35,22 @@ class Fixed {
 
 Fixed::Fixed()
 {
-	this->fpn = 0;
 	std::cout << "Default constructor invoked" << std::endl;
+	this->fpn = 0;
 }
 
 Fixed::Fixed(const Fixed &primary)
 {
-	this->fpn = primary.fpn;
 	std::cout << "Copy constructor invoked" << std::endl;
+	this->fpn = primary.getRawBits();
 }
 
 Fixed &Fixed::operator=(const Fixed &primary)
 {
 	//protection if self-assignment to avoid unnecessary work
-	if (this != &primary)
-		this->fpn = primary.fpn;
 	std::cout << "Copy assignment operator invoked" << std::endl;
+	if (this != &primary)
+		this->fpn = primary.getRawBits();
 	return (*this);
 }
 
@@ -62,8 +62,8 @@ int Fixed::getRawBits() const
 
 void Fixed::setRawBits(int const raw)
 {
-	this->fpn = raw;
 	std::cout << "setRawBits member function called" << std::endl;
+	this->fpn = raw;
 }
 
 Fixed::~Fixed()
