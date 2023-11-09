2c8b2c677f27451a2c33485da64e645adc594538
FRUIT-DU-DEMON <hlabouit@student.1337.ma>
2023-11-09 23:39:28 +0000

    Fixed class implemented

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hlabouit <hlabouit@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/05 17:58:23 by hlabouit          #+#    #+#             */
-/*   Updated: 2023/11/05 23:20:32 by hlabouit         ###   ########.fr       */
+/*   Updated: 2023/11/09 22:19:31 by hlabouit         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -22,11 +22,65 @@
 #include <cstdlib>
 #include <cmath>
 
-int main() {
-    float floatValue = 3.09;
-    float roundedValue = roundf(floatValue);
-    std::cout << "Original value: " << floatValue << std::endl;
-    std::cout << "Rounded value: " << roundedValue << std::endl;
+///////////////////// class //////////////////////
+class Fixed {
+	private :
+		int fpn; //the big number is the binary representation of the fpn which is an encoded form
+		static const int fpn_binary = 8;
+	public :
+		Fixed();
+		Fixed(const Fixed &primary);
+		Fixed &operator=(const Fixed &primary);
+        //overloaded <<
+        Fixed(const int int_val);
+        Fixed(const float float_val);
+        float toFloat() const;
+        int toInt() const;
+		int getRawBits() const;
+		void setRawBits(int const raw);
+		~Fixed();
+};
+///////////////////// class //////////////////////
+Fixed::Fixed()
+{
+	std::cout << "Default constructor invoked" << std::endl;
+	this->fpn = 0;
+}
 
-    return 0;
+Fixed::Fixed(const Fixed &primary)
+{
+	std::cout << "Copy constructor invoked" << std::endl;
+	this->fpn = primary.getRawBits();
+}
+
+Fixed &Fixed::operator=(const Fixed &primary)
+{
+	//protection if self-assignment to avoid unnecessary work
+	std::cout << "Copy assignment operator invoked" << std::endl;
+	if (this != &primary)
+		this->fpn = primary.getRawBits();
+	return (*this);
+}
+
+int Fixed::getRawBits() const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (this->fpn);
+}
+
+void Fixed::setRawBits(int const raw)
+{
+	std::cout << "setRawBits member function called" << std::endl;
+	this->fpn = raw;
+}
+
+Fixed::~Fixed()
+{
+	std::cout<< "Destructor invoked" << std::endl;
+}
+
+
+int main()
+{
+    
 }
