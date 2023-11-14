13710ef41a431c4f690557c6270c6cf8b308b90d
FRUIT-DU-DEMON <hlabouit@student.1337.ma>
2023-11-14 02:47:28 +0000

    ocf implemented

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hlabouit <hlabouit@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 23:00:16 by hlabouit          #+#    #+#             */
-/*   Updated: 2023/11/13 23:37:09 by hlabouit         ###   ########.fr       */
+/*   Updated: 2023/11/14 02:46:12 by hlabouit         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -26,16 +26,57 @@
 class ClapTrap {
 	private :
 		std::string name;
-		int hit_points = 10;
-		int energy_points = 10;
-		int attack_damage = 0;
+		int hit_points;
+		int energy_points;
+		int attack_damage;
 	public :
 		ClapTrap();
 		ClapTrap(const ClapTrap &primary);
 		ClapTrap &operator=(const ClapTrap &primary);
+		ClapTrap(const std::string &which_name);
+		void attack(const std::string &target);
+		void takeDamage(unsigned int amount); 
+		void beRepaired(unsigned int amount);
 		~ClapTrap();
-	
 };
+
+ClapTrap::ClapTrap()
+{
+	std::cout<< "Default constructor invoked" << std::endl;
+	this->name = "unknown";
+	this->hit_points = 10;
+	this->energy_points = 10;
+	this->attack_damage = 0;
+}
+
+ClapTrap::ClapTrap(const ClapTrap &primary)
+{
+	std::cout<< "Copy constructor invoked" << std::endl;
+	*this = primary;
+}
+
+ClapTrap &ClapTrap::operator=(const ClapTrap &primary)
+{
+	std::cout<< "Copy assignment operator invoked" << std::endl;
+	if (this != &primary)
+	{
+		this->name = primary.name;
+		this->hit_points = primary.hit_points;
+		this->energy_points = primary.energy_points;
+		this->attack_damage = primary.attack_damage;
+	}
+	return (*this);
+}
+
+ClapTrap::ClapTrap(const std::string &which_name)
+{
+	
+}
+
+
+
+
+
 
 
 int main()
