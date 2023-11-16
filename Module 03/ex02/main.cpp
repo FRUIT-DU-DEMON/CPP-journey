855ec0b7fde82600d78bb087594bace15041a62b
FRUIT-DU-DEMON <hlabouit@student.1337.ma>
2023-11-16 21:50:23 +0000

    Module 03 done

--- Module 03/ex02/main.cpp
+++ Module 03/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hlabouit <hlabouit@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 01:45:09 by hlabouit          #+#    #+#             */
-/*   Updated: 2023/11/16 06:08:33 by hlabouit         ###   ########.fr       */
+/*   Updated: 2023/11/16 21:48:56 by hlabouit         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -14,7 +14,7 @@
 
 int main()
 {
-    FragTrap Cyborg("Franky");
+    FragTrap Cyborg("Franky");//a subobject of ClapTrap created
 	// Cyborg.attack("CP9");
 	// ClapTrap Pacifista(Cyborg);
 	// Pacifista.attack("Kizaru");
