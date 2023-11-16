208af1cb2364a027feae6d57bd705c80f52b6edc
FRUIT-DU-DEMON <hlabouit@student.1337.ma>
2023-11-16 03:19:12 +0000

    ex01/day03---->DONE

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hlabouit <hlabouit@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 02:03:23 by hlabouit          #+#    #+#             */
-/*   Updated: 2023/11/16 01:39:42 by hlabouit         ###   ########.fr       */
+/*   Updated: 2023/11/16 03:19:02 by hlabouit         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -18,5 +18,7 @@ int main()
     ScavTrap Cyborg("Franky");
 	Cyborg.attack("CP9");
 	// Pacifista.attack("Kizaru");
+	// Pacifista = Cyborg;
+	// Pacifista.attack("Ivankov");
 	Cyborg.guardGate();
 }
