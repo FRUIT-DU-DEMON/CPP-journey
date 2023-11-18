b456a834570634e055b73a503134f8eaac7d7c4a
FRUIT-DU-DEMON <hlabouit@student.1337.ma>
2023-11-18 00:29:27 +0000

    BaseClass Animal members implemented

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hlabouit <hlabouit@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 20:46:37 by hlabouit          #+#    #+#             */
-/*   Updated: 2023/11/17 22:49:21 by hlabouit         ###   ########.fr       */
+/*   Updated: 2023/11/18 00:28:56 by hlabouit         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -30,13 +30,50 @@ class Animal {
 		Animal(const Animal &primary);
 		Animal &operator=(const Animal &primary);
 		Animal(const std::string &which_type);
-		virtual void makeSound();
-		std::string getType();
+		virtual void makeSound() const;
+		const std::string &getType() const;
 		~Animal();
 };
+
+Animal::Animal()
+{
+	std::cout<< "Base class Animal default constructor called" << std::endl;
+	this->type = "l'ex";
+}
+
+Animal::Animal(const Animal &primary)
+{
+	std::cout<< "Base class Animal copy constructor called" << std::endl;
+	this->type = primary.type;
+}
+
+Animal &Animal::operator=(const Animal &primary)
+{
+	std::cout<< "Base class Animal copy assignment operator called" << std::endl;
+	if (this != &primary)
+		this->type = primary.type;
+	return (*this);
+}
+
+Animal::Animal(const std::string &which_type)
+{
+	std::cout<< "Animal " << which_type << " is born" << std::endl;
+	this->type = which_type;
+}
+
+void Animal::makeSound() const
+{
+	std::cout<< this->type << " is saying something! but who cares" << std::endl;
+}
+
+const std::string &Animal::getType() const
+{
+	return (this->type);
+}
 
 
 int main()
 {
-	
+	Animal ex;
+	ex.makeSound();
 }
