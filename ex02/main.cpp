fc78f5c7e39117134dfc23d3e99fefe909841a6d
FRUIT-DU-DEMON <hlabouit@student.1337.ma>
2023-11-23 17:11:40 +0100

    files from ex01 imported

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: hlabouit <hlabouit@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/22 17:55:30 by hlabouit          #+#    #+#             */
-/*   Updated: 2023/11/23 16:46:14 by hlabouit         ###   ########.fr       */
+/*   Created: 2023/11/23 16:48:46 by hlabouit          #+#    #+#             */
+/*   Updated: 2023/11/23 17:06:15 by hlabouit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,33 +17,7 @@
 #include"WrongCat.hpp"
 #include"Brain.hpp"
 
-// void fun()
-// {
-//     system("leaks brain");
-// }
-
-
 int main()
 {
-    // atexit(fun);
     
-    const Animal* j = new Dog();
-    const Animal* i = new Cat();
-    
-    delete j;
-    delete i;
-    
-    Animal *arr[6];
-    
-    for (int i = 0; i < 6; i++)
-    {
-        if (i > 2)
-            arr[i] = new Cat();
-        else
-            arr[i] = new Dog();
-    }
-    for (int i = 0; i < 6; i++)
-        delete arr[i];
-
-    return 0;
 }
